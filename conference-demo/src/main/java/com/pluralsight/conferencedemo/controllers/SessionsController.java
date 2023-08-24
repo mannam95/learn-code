7268b8b6efbda3cd2204d32465305c41006ad328
Srinath Mannam <mvsrinath2009@gmail.com>
2023-08-24 14:00:30 +0200

    updated controller with service

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -1,44 +1,46 @@
 package com.pluralsight.conferencedemo.controllers;
 
+import com.pluralsight.conferencedemo.dto.session.CreateSessionDto;
+import com.pluralsight.conferencedemo.dto.session.UpdateSessionDto;
 import com.pluralsight.conferencedemo.models.Session;
-import com.pluralsight.conferencedemo.repositories.SessionRepository;
-import org.springframework.beans.BeanUtils;
+import com.pluralsight.conferencedemo.services.SessionService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/sessions")
+@Validated
 public class SessionsController {
     @Autowired
-    private SessionRepository sessionRepository;
+    private SessionService sessionService;
 
     @GetMapping
     public List<Session> list() {
-        return sessionRepository.findAll();
+        return sessionService.getAllSessions();
     }
 
     @GetMapping
     @RequestMapping("{id}")
     public Session get(@PathVariable Long id) {
-        return sessionRepository.getOne(id);
+        return sessionService.getSessionById(id);
     }
 
     @PostMapping
-    public Session create(@RequestBody final Session session) {
-        return sessionRepository.saveAndFlush(session);
+    public Session create(@RequestBody @Valid CreateSessionDto session) {
+        return sessionService.createNewSession(session);
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable Long id) {
-        sessionRepository.deleteById(id);
+        sessionService.deleteBySessionId(id);
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.PUT)
-    public Session update(@PathVariable Long id, @RequestBody Session session) {
-        Session existingSession = sessionRepository.getOne(id);
-        BeanUtils.copyProperties(session, existingSession, "session_id");
-        return sessionRepository.saveAndFlush(existingSession);
+    public Session update(@PathVariable Long id, @RequestBody @Valid UpdateSessionDto session) {
+        return sessionService.updateSession(id, session);
     }
 }
--- conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
@@ -1,44 +1,46 @@
 package com.pluralsight.conferencedemo.controllers;
 
+import com.pluralsight.conferencedemo.dto.speaker.CreateSpeakerDto;
+import com.pluralsight.conferencedemo.dto.speaker.UpdateSpeakerDto;
 import com.pluralsight.conferencedemo.models.Speaker;
-import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
-import org.springframework.beans.BeanUtils;
+import com.pluralsight.conferencedemo.services.SpeakerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/speakers")
+@Validated
 public class SpeakersController {
     @Autowired
-    private SpeakerRepository speakerRepository;
+    private SpeakerService speakerService;
 
     @GetMapping
     public List<Speaker> list() {
-        return speakerRepository.findAll();
+        return speakerService.getAllSpeakers();
     }
 
     @GetMapping
     @RequestMapping("{id}")
     public Speaker get(@PathVariable Long id) {
-        return speakerRepository.getOne(id);
+        return speakerService.getSpeakerById(id);
     }
 
     @PostMapping
-    public Speaker create(@RequestBody final Speaker speaker) {
-        return speakerRepository.saveAndFlush(speaker);
+    public Speaker create(@RequestBody @Valid CreateSpeakerDto speaker) {
+        return speakerService.createNewSpeaker(speaker);
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable Long id) {
-        speakerRepository.deleteById(id);
+        speakerService.deleteBySpeakerId(id);
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.PUT)
-    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
-        Speaker existingSpeaker = speakerRepository.getOne(id);
-        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
-        return speakerRepository.saveAndFlush(existingSpeaker);
+    public Speaker update(@PathVariable Long id, @RequestBody @Valid UpdateSpeakerDto speaker) {
+        return speakerService.updateSpeaker(id, speaker);
     }
 }
