cb6974b41871968449f6c16277bad4600b8af485
Srinath Mannam <mvsrinath2009@gmail.com>
2023-08-24 14:01:20 +0200

    changed to lombok

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Session.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -3,18 +3,33 @@ package com.pluralsight.conferencedemo.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity(name = "sessions")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Session {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long session_id;
-    private String session_name;
-    private String session_description;
-    private Integer session_length;
+    @Column(name = "session_id")
+    private Long sessionId;
+
+    @Column(name = "session_name")
+    private String sessionName;
+
+    @Column(name = "session_description")
+    private String sessionDescription;
+
+    @Column(name = "session_length")
+    private Integer sessionLength;
 
     @ManyToMany
     @JoinTable(
@@ -23,47 +38,4 @@ public class Session {
             inverseJoinColumns = @JoinColumn(name = "speaker_id")
     )
     private List<Speaker> speakers;
-    public Session() {
-
-    }
-
-    public Long getSession_id() {
-        return session_id;
-    }
-
-    public void setSession_id(Long session_id) {
-        this.session_id = session_id;
-    }
-
-    public String getSession_name() {
-        return session_name;
-    }
-
-    public void setSession_name(String session_name) {
-        this.session_name = session_name;
-    }
-
-    public String getSession_description() {
-        return session_description;
-    }
-
-    public void setSession_description(String session_description) {
-        this.session_description = session_description;
-    }
-
-    public Integer getSession_length() {
-        return session_length;
-    }
-
-    public void setSession_length(Integer session_length) {
-        this.session_length = session_length;
-    }
-
-    public List<Speaker> getSpeakers() {
-        return speakers;
-    }
-
-    public void setSpeakers(List<Speaker> speakers) {
-        this.speakers = speakers;
-    }
 }
