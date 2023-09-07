14b89b179f34557f3e4abc8aa37535bc015b3403
Srinath Mannam <mvsrinath2009@gmail.com>
2023-09-07 14:20:02 +0200

    example-3 in method overloading

---
+++ interview-tasks/src/MethodOverLoading/TestMethod3.java
@@ -0,0 +1,15 @@
+package MethodOverLoading;
+
+public class TestMethod3 {
+
+    static void add(double d, double a){
+        System.out.println("Addition: "+(a+d));
+    }
+    static void add(int d, double a){
+        System.out.println("Addition: "+(a+d));
+    }
+
+    public static void main(String... str){
+        add(3,5);
+    }
+}
