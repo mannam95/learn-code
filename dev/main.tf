785c0403a3b26a481c185315cbf3bad93cebcc67
Srinath Mannam <mvsrinath2009@gmail.com>
2023-09-10 11:38:33 +0200

    organized to dev environment

--- dev/main.tf
+++ dev/main.tf
@@ -1,5 +1,5 @@
 module "dynamodb" {
-  source = "./dynamodb"
+  source = "../dynamodb"
   dynamodb_table_name = var.dynamodb_table_name
   dynamodb_table_billing_mode = var.dynamodb_table_billing_mode
   dynamodb_table_tags = var.dynamodb_table_tags
@@ -12,14 +12,14 @@ module "dynamodb" {
 }
 
 module "lambda" {
-  source = "./lambda"
+  source = "../lambda"
   lambda_execution_role_name = var.lambda_execution_role_name
   lambda_function_name = var.lambda_function_name
   basic_dynamodb_table_arn = module.dynamodb.basic_dynamodb_table_arn
 }
 
 module "api-gateway" {
-  source = "./api-gateway"
+  source = "../api-gateway"
   my_api_gateway_rest_api_name = var.my_api_gateway_rest_api_name
   my_lambda_function_name = module.lambda.my_lambda_function_name
   my_lambda_function_invoke_arn = module.lambda.my_lambda_function_invoke_arn
