e1335239458b5d09805a6deb35c2abbd45a3f71c
Srinath Mannam <srinath.mannam@yellow-ant.de>
2023-09-25 20:44:52 +0200

    integrated the ocr text api

--- backend/util.py
+++ backend/util.py
@@ -1,9 +1,24 @@
+import easyocr
+
+reader = easyocr.Reader(['en','de'])
+
+def get_detected_text(image_path):
+    try:
+        bounds = reader.readtext(image_path)
+
+        if len(bounds) == 0:
+            return "No text detected1"
+        else:
+            detected_texts = [bound[1] for bound in bounds]
+
+            result_string = " \n ".join(detected_texts)
+
+            return result_string
+    except:
+        return "No text detected2"
+
 def get_translated_text():
     try:
-        return {
-            'detectedText': "Sample",
-            'translatedText': "Deutsch",
-            'detectedLanguage': "GE",
-        }
+        return get_detected_text("./images/dummy1.png")
     except:
-        return ['empty_list']
+        return "No text detected3"
