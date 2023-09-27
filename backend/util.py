6662a14406f779adeb804ba452fd07a13e9434dc
Srinath Mannam <srinath.mannam@yellow-ant.de>
2023-09-27 12:10:55 +0200

    getting the image dynamically

--- backend/util.py
+++ backend/util.py
@@ -1,48 +1,80 @@
-import os, io
+import os
 from google.cloud import vision
-import pandas as pd
 
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"/google/apijson/eminent-bond-278322-2cfd6c241bfa.json"
 
-def detect_text_from_image(image_path):
+def get_img_content(api_request):
+
+    is_file = False
+    img_content = "No text detected"
+
+    try:
+        print("API Request:", api_request)
+
+        if 'fileName' not in api_request.files:
+            print("error: No file part")
+        
+        file = api_request.files['fileName']
+        if file.filename == '':
+            print("error: No selected file")
+        
+        img_content = file.read()
+        is_file = True
+
+        return (is_file, img_content)
+
+    except:
+        return (is_file, img_content)
+
+
+def detect_text_from_image(file_content):
     detected_text = "No text detected"
     try: 
-      """Detects text in the file located in Google Cloud Storage or on the Web."""
+        """Detects text in the file located in Google Cloud Storage or on the Web."""
 
-      file_name = os.path.abspath(image_path)
+        #   file_name = os.path.abspath(image_path)
 
-      with io.open(file_name, 'rb') as image_file:
-          content = image_file.read()
+        #   with io.open(file_name, 'rb') as image_file:
+        #       content = image_file.read()
 
-      # construct an image instance
-      client = vision.ImageAnnotatorClient()
-      image = vision.Image(content=content)
+        content = file_content
 
-      """
-      # or we can pass the image url
-      image = vision.types.Image()
-      image.source.image_uri = 'https://edu.pngfacts.com/uploads/1/1/3/2/11320972/grade-10-english_orig.png'
-      """
+        # construct an image instance
+        client = vision.ImageAnnotatorClient()
+        image = vision.Image(content=content)
 
-      response = client.text_detection(image=image)
-      texts = response.text_annotations
+        """
+        # or we can pass the image url
+        image = vision.types.Image()
+        image.source.image_uri = 'https://edu.pngfacts.com/uploads/1/1/3/2/11320972/grade-10-english_orig.png'
+        """
 
-      for text in texts:
-          detected_text = text.description
-          break
+        response = client.text_detection(image=image)
 
-      if response.error.message:
-          raise Exception(
-              "{}\nFor more info on error messages, check: "
-              "https://cloud.google.com/apis/design/errors".format(response.error.message)
-          )
-      return detected_text
+        texts = response.text_annotations
+
+        for text in texts:
+            detected_text = text.description
+            break
+
+        if response.error.message:
+            print(response.error.message)
+            raise Exception(
+                "{}\nFor more info on error messages, check: "
+                "https://cloud.google.com/apis/design/errors".format(response.error.message)
+            )
+        return detected_text
     except:
-      return detected_text
+        return detected_text
 
-def get_translated_text():
-    try:
-        return detect_text_from_image("./images/dummy1.png")
+def get_translated_text(api_request):
+    try: 
+        is_file, file_content = get_img_content(api_request)
+
+        if is_file == False:
+            return file_content
+        
+        return detect_text_from_image(file_content)
     except:
         return "No text detected"
