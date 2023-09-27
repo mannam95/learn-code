5d71568c0baf2bc9866e72fae90c81f2be0a3644
Srinath Mannam <srinath.mannam@yellow-ant.de>
2023-09-27 09:52:24 +0200

    fixed code according to google vision api

--- backend/util.py
+++ backend/util.py
@@ -1,24 +1,48 @@
-import easyocr
+import os, io
+from google.cloud import vision
+import pandas as pd
 
-reader = easyocr.Reader(['en','de'])
 
-def get_detected_text(image_path):
-    try:
-        bounds = reader.readtext(image_path)
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"eminent-bond-278322-2cfd6c241bfa.json"
 
-        if len(bounds) == 0:
-            return "No text detected1"
-        else:
-            detected_texts = [bound[1] for bound in bounds]
+def detect_text_from_image(image_path):
+    detected_text = "No text detected"
+    try: 
+      """Detects text in the file located in Google Cloud Storage or on the Web."""
 
-            result_string = " \n ".join(detected_texts)
+      file_name = os.path.abspath(image_path)
 
-            return result_string
+      with io.open(file_name, 'rb') as image_file:
+          content = image_file.read()
+
+      # construct an image instance
+      client = vision.ImageAnnotatorClient()
+      image = vision.Image(content=content)
+
+      """
+      # or we can pass the image url
+      image = vision.types.Image()
+      image.source.image_uri = 'https://edu.pngfacts.com/uploads/1/1/3/2/11320972/grade-10-english_orig.png'
+      """
+
+      response = client.text_detection(image=image)
+      texts = response.text_annotations
+
+      for text in texts:
+          detected_text = text.description
+          break
+
+      if response.error.message:
+          raise Exception(
+              "{}\nFor more info on error messages, check: "
+              "https://cloud.google.com/apis/design/errors".format(response.error.message)
+          )
+      return detected_text
     except:
-        return "No text detected2"
+      return detected_text
 
 def get_translated_text():
     try:
-        return get_detected_text("./images/dummy1.png")
+        return detect_text_from_image("./images/dummy1.png")
     except:
-        return "No text detected3"
+        return "No text detected"
