4b99e4b99d034a79543de9198181804b0c13350f
Srinath Mannam <srinath.mannam@yellow-ant.de>
2023-09-27 12:10:39 +0200

    removed unused imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 import backend.util as util
 
 app = Flask(__name__)
@@ -8,15 +8,16 @@ def home():
     return "Hello World!"
 
 
-@app.route('/get_translated_text', methods=['GET'])
+@app.route('/get_translated_text', methods=['POST'])
 def get_translated_text():
     response = jsonify({
-        'trans_text': util.get_translated_text()
+        'trans_text': util.get_translated_text(request)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    print("Starting Python Flask Server For Text Detection and Translation...")
+    app.run(debug=True, host='0.0.0.0', port=80)
     # app.run(debug=True)
