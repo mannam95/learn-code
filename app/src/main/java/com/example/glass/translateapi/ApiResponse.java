d98c8149eceb4600ab696be8a2f3a5dad22ab87f
Srinath Mannam <srinath.mannam@yellow-ant.de>
2023-09-27 17:57:25 +0200

    added a api response class to map data

---
+++ app/src/main/java/com/example/glass/translateapi/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.example.glass.translateapi;
+
+public class ApiResponse {
+    private String trans_text;
+
+    // Getter
+    public String getTransText() {
+        return trans_text;
+    }
+
+    // Setter (optional, depending on whether you need it)
+    public void setTransText(String trans_text) {
+        this.trans_text = trans_text;
+    }
+}
