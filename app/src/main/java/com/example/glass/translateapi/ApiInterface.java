10e64fa501e10896d0f5a4156ce28a7ac46e0289
Srinath Mannam <mvsrinath2009@gmail.com>
2023-09-27 18:01:41 +0200

    updated get method interface

--- app/src/main/java/com/example/glass/translateapi/ApiInterface.java
+++ app/src/main/java/com/example/glass/translateapi/ApiInterface.java
@@ -1,9 +1,14 @@
 package com.example.glass.translateapi;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
-import retrofit2.http.GET;
+import retrofit2.http.Body;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface ApiInterface {
-    @GET("todos/1")
-    Call<Object> getData();
+    @POST("get_translated_text")
+    Call<Object> getData(@Body RequestBody requestBody);
 }
